af4a176c14da6b818911223003897bab4b821882
DennisEckler <eckler.d95@gmail.com>
2022-11-15 10:55:50 +0100

    updated routing and first views

--- LagerApp/urls.py
+++ LagerApp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name ='LagerApp'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('article/', views.article, name='article'),
+    path('lagerliste/', views.lagerliste, name='lagerliste'),
+    path('kennzahlen/', views.kennzahlen, name='kennzahlen'),
+    path('lagerliste/<int:storage_id>/', views.lagerplatz, name='lagerplatz'),
 ]
--- LagerApp/views.py
+++ LagerApp/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-from .models import Article
+from django.shortcuts import render,get_object_or_404
+from .models import Article, Storage, Kennzahlen
 
 # Create your views here.
 
-def index(request):
+def home(request):
+    return render(request, 'LagerApp/home.html')
+
+def article(request):
     article_list = Article.objects.all()
     context = {'article_list': article_list,}
-    return render(request, 'LagerApp/index.html', context)
+    return render(request, 'LagerApp/articles.html', context)
+
+def lagerliste(request):
+    lager_list = Storage.objects.all()
+    context = {'lager_list': lager_list}
+    return render(request, 'LagerApp/lagerliste.html', context)
+
+def kennzahlen(request):
+    return render(request, 'LagerApp/kennzahlen.html')
+
+def lagerplatz(request, storage_id):
+    lagerplatz = get_object_or_404(Storage, pk=storage_id)
+    context = {'lager': lagerplatz}
+    return render(request, 'LagerApp/lagerplatz.html', context)
