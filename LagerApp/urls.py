72d202eeaa178cee8d9391fb7fc86257ff44422c
DennisEckler <eckler.d95@gmail.com>
2022-11-22 10:52:23 +0100

    added styles with bootstrap

--- LagerApp/urls.py
+++ LagerApp/urls.py
@@ -5,8 +5,9 @@ app_name ='LagerApp'
 urlpatterns = [
     path('', views.home, name='home'),
     path('article/', views.article, name='article'),
+    path('article/<int:article_id>', views.detail, name='detail'),
     path('lagerliste/', views.lagerliste, name='lagerliste'),
     path('kennzahlen/', views.kennzahlen, name='kennzahlen'),
     path('lagerliste/<int:storage_id>/', views.lagerplatz, name='lagerplatz'),
-    path('article/<int:article_id>/buchen', views.buchen , name='buchen'),
+    path('article/<int:article_id>/buchen', views.buchen, name='buchen'),
 ]
--- LagerApp/views.py
+++ LagerApp/views.py
@@ -13,6 +13,11 @@ def article(request):
     context = {'article_list': article_list,}
     return render(request, 'LagerApp/articles.html', context)
 
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    context = {'article': article,}
+    return render(request, 'LagerApp/article_detail.html', context)
+
 def lagerliste(request):
     lager_list = Storage.objects.all()
     context = {'lager_list': lager_list}
@@ -47,10 +52,21 @@ def buchen(request, article_id):
     if request.method == 'POST':
         article = Article.objects.get(pk=article_id)
         lagerplatz = get_object_or_404(Storage, pk=article.storage.id)
+        kennzahl = get_object_or_404(Kennzahlen, pk=article.id)
         article_list = Article.objects.filter(storage=lagerplatz.id)
         try:
             amount = request.POST['menge']
             article.anzahl += int(amount)
+
+            if (int(amount) > 0):
+                kennzahl.gewinn += (int(amount) * article.ekpreis) * -1
+
+            if (int(amount) < 0):
+                kennzahl.umsatz += (int(amount) * article.vkpreis) * -1
+                kennzahl.abgänge += int(amount) * -1
+                kennzahl.gewinn += (int(amount) * article.vkpreis) * -1
+
+
 
         except ValueError as e:
             return render(request, 'LagerApp/lagerplatz.html', {
@@ -60,4 +76,5 @@ def buchen(request, article_id):
             })
         else:
             article.save()
+            kennzahl.save()
             return HttpResponseRedirect(reverse('LagerApp:lagerplatz', args=(lagerplatz.id,)))
